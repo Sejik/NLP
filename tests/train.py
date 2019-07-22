1a465962a391e71a2600473dc18e488b7ac315c9
Sejik Park <sejik6307@gmail.com>
2019-07-22 23:32:01 +0900

    travis Check

---
+++ tests/train.py
@@ -0,0 +1,37 @@
+def test_travis():
+    print("testing Travis CI")
+
+if __name__ == "__main__":
+    """
+    experiment_setting
+    experiment_operation
+    """
+    test_travis()
+    #setting = experiment_setting()
+    #experiment_operation()
+
+# KorQUAD
+# 1. Training set: Train.json
+# 2. Dev set: dev.json
+# 3. Evaluate.py: evaluation script
+# 4. Dms: sample prediction file
+
+# experiment_setting( considering input?? )
+# input: mode, config
+# mode:
+"""
+class Mode:
+    # Experiment Flag class
+    
+    TRAIN = "train"
+    EVAL = "eval"
+    INFER_EVAL = "infer_eval"
+    PREDICT = "predict"
+    MACHINE = "machine"
+"""
+# config:
+"""
+    paser = argparse.ArugmentParser(formatter_class=RawTextHelpFormatter)
+    evaluate(parser)
+"""
+
